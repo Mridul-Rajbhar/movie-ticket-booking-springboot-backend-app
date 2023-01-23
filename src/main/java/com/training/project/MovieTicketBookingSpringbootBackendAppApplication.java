bba793e934294512dc19f5b3b0d9ea29ce32152e
Mridul Rajbhar <mridulrajbhar12@gmail.com>
2023-01-23 18:49:51 +0530

    initial commit

---
+++ src/main/java/com/training/project/MovieTicketBookingSpringbootBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingSpringbootBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSpringbootBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/project/MovieTicketBookingSpringbootBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingSpringbootBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
